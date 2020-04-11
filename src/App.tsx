db8c28223994b31a730c8619d05c706ed4556c63
lu97chi <lu97is@gmail.com>
2020-04-10 20:35:06 -0600

    need to fix bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import './App.css';
 import { Button } from '@material-ui/core';
+import './App.css';
 
 function App() {
   const { t, i18n } = useTranslation();
---
+++ src/components/Button/styled.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ContainerButton = styled.div`
+    background: red;
+`;
